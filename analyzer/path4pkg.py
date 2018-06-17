fb67de78d7df6643587ebe625cc0ee109d34393b
Oksana Shadura <oksana.shadura@cern.ch>
2018-06-17 18:19:30 +0200

    Moving zip function to separate class (WIP)

--- analyzer/path4pkg.py
+++ analyzer/path4pkg.py
@@ -1,10 +1,17 @@
-#!/usr/bin/env python
-
+""" A Path4pkg class
+Experiments on trying to generate package for ROOT PM
+"""
 import os
 
-def path4pkg(dirname, directory=''):
-    dirname = dirname.lower()
-    for root, dirs, names in os.walk(directory):
-        if dirname in dirs:
-            return os.path.join(root, dirname)
-    raise 'We work only with ROOT packages by now'
+class Path4pkg(object):
+    """docstring for Path4pkg."""
+    def __init__(self, arg=None):
+        super(Path4pkg, self).__init__()
+        self.arg = arg
+
+    def path4pkg(self, dirname, directory=''):
+        dirname = dirname.lower()
+        for root, dirs, names in os.walk(directory):
+            if dirname in dirs:
+                return os.path.join(root, dirname)
+        raise 'We work only with ROOT packages by now'
