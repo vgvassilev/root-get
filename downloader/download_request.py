9f2c93e394316970003380882512a492d967d92c
Oksana Shadura <oksana.shadura@cern.ch>
2018-06-17 18:15:50 +0200

    Starting to work on downloader classes

---
+++ downloader/download_request.py
@@ -0,0 +1,5 @@
+class Downloader_response(object):
+    """docstring for Downloader_response."""
+    def __init__(self, arg):
+        super(Downloader_response, self).__init__()
+        self.arg = arg
---
+++ downloader/download_response.py
@@ -0,0 +1,6 @@
+class Downloader_request(object):
+    """docstring for Downloader_request."""
+    def __init__(self, arg):
+        super(Downloader_request, self).__init__()
+        self.arg = arg
+        
