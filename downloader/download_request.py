e8f8633bc4047dd8325bc9e9dde104e613fbe320
Oksana Shadura <oksana.shadura@cern.ch>
2018-06-19 18:54:03 +0200

    Refreshing downloader, very simple operations

--- downloader/download_request.py
+++ downloader/download_request.py
@@ -1,5 +1,35 @@
-class Downloader_response(object):
-    """docstring for Downloader_response."""
-    def __init__(self, arg):
-        super(Downloader_response, self).__init__()
-        self.arg = arg
+import wget
+import urllib2
+from git import Repo
+import re
+
+class Downloader_request(object):
+    """Constructor for Downloader_request."""
+    def __init__(self, url, dir):
+        super(Downloader_request, self).__init__()
+        self.url = url
+        self.dir = dir
+
+    # FIXME: next step is to move to PyGithub
+    def download_github(self):
+        Repo.clone_from(self.url, self.dir)
+        print("Cloning from github %s", self.url)
+
+    def download_zip(self):
+        wget.download(self.url, self.dir)
+        print("Downloading from http/https %s", self.url)
+
+    def resolving_download(self):
+        request = urllib2.Request(self.url)
+        request.get_method = lambda : 'HEAD'
+        try:
+            response = urllib2.urlopen(request)
+            return True
+        except urllib2.HTTPError:
+            return False
+        # check if we have github link
+        match = re.search('github', self.url)
+        if match:
+            download_github(self.url, self.dir)
+        else:
+            download_zip(self.url, self.dir)
--- downloader/download_response.py
+++
@@ -1,6 +0,0 @@
-class Downloader_request(object):
-    """docstring for Downloader_request."""
-    def __init__(self, arg):
-        super(Downloader_request, self).__init__()
-        self.arg = arg
-        
