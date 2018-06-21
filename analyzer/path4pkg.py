868753cd9711f25a15068b690fb955878e9da895
Oksana Shadura <oksana.shadura@cern.ch>
2018-06-21 20:41:01 +0200

    Extending walking routine to be able to select minlevel and max level for walking

--- analyzer/path4pkg.py
+++ analyzer/path4pkg.py
@@ -9,16 +9,24 @@ class Path4pkg(object):
         super(Path4pkg, self).__init__()
         self.arg = arg
 
-    def path4pkg(self, dirname, directory=''):
+    def path4pkg(self, dirname, directory='', mindepth=2, maxdepth=float('3')):
+        directory = os.path.normcase(directory)
         dirname = dirname.lower()
         non_acceptable_dirs = set(['tutorials', 'test', 'interpreter', 'dictpch'])
         directory_exceptions = set(['rfio', 'io'])
-        for root, dirs, names in os.walk(directory):
+        root_depth = directory.rstrip(os.path.sep).count(os.path.sep) - 1
+        for dirpath, dirs, names in os.walk(directory):
             dirs[:] = [d for d in dirs if d not in non_acceptable_dirs]
-            if((dirname in dirs) or (dirname.strip("io") in dirs)):
-                if(dirname in directory_exceptions):
-                    return os.path.join(root, dirname)
-                else:
-                    return os.path.join(root, dirname.strip("io"))
-            #elif dirname not in directory_exceptions in dirs
-        raise Exception('We work only with ROOT packages by now')
+            depth = dirpath.count(os.path.sep) - root_depth
+            if mindepth <= depth <= maxdepth:
+                if((dirname in dirs) or (dirname.strip("io") in dirs)):
+                    if(dirname in directory_exceptions):
+                        print(dirname)
+                        return os.path.join(dirpath, dirname)
+                    else:
+                        print(dirname)
+                        return os.path.join(dirpath, dirname.strip("io"))
+                #raise Exception('We work only with ROOT modules by now')
+            elif depth > maxdepth:
+                del dirs[:] # too deep, don't recurse
+            #raise Exception('Exist only package with such name!')
