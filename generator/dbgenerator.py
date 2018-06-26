ae9c5066fcdbf61bd66c1827b67a69a98e6b8348
kiryteo <capnoi8@gmail.com>
2018-06-26 15:50:55 +0530

    added routine to avoid repetition of data in manifest.yml file

--- generator/dbgenerator.py
+++ generator/dbgenerator.py
@@ -21,61 +21,72 @@ class Dbgenerator(object):
 		rule_targets = re.compile('.*targets:.*')
 		rule_deps = re.compile('.*deps:.*')
 
-		for subdir, dirs, files in os.walk(rootdir):
-			for file in files:
-				if file == "module.yml":
-					fp = os.path.join(subdir, file)
-					with open(fp) as t:
-						fl = t.read()
-						names = rule_name.findall(fl)
-						parcing_rule_name = [x.strip(' name: ') for x in names]
-						if parcing_rule_name:
-							fl2 = open("manifest.yml", 'a')
-							fl2.write(",".join(parcing_rule_name))
-							fl2.write(":")
-							fl2.write("\n")
-						rule_package_url_check = rule_package_url.findall(fl)
-						if rule_package_url_check:
-							fl2 = open("manifest.yml", 'a')
-							fl2.write(",".join(rule_package_url_check))
+		if os.path.isfile("manifest.yml"):
+			infile = "manifest.yml"
+			s = set()
+			with open('temp.yml', 'w') as out:
+				for line in open(infile):
+					if line not in s:
+						out.write(line)
+						s.add(line)
+		else:
+			for subdir, dirs, files in os.walk(rootdir):
+				for file in files:
+					if file == "module.yml":
+						fp = os.path.join(subdir, file)
+						with open(fp) as t:
+							fl = t.read()
+							names = rule_name.findall(fl)
+							parcing_rule_name = [x.strip(' name: ') for x in names]
+							if parcing_rule_name:
+								fl2 = open("manifest.yml", 'a')
+								fl2.write(",".join(parcing_rule_name))
+								fl2.write(":")
+								fl2.write("\n")
+							rule_tag_check = rule_tag.findall(fl)
+							if rule_tag_check:
+								fl2 = open("manifest.yml", 'a')
+								fl2.write(",".join(rule_tag_check))
 							#fl2.write(":")
-							fl2.write("\n")
-						rule_tag_check = rule_tag.findall(fl)
-						if rule_tag_check:
-							fl2 = open("manifest.yml", 'a')
-							fl2.write(",".join(rule_tag_check))
-							#fl2.write(":")
-							fl2.write("\n")
-						rule_path_check = rule_path.findall(fl)
-						if rule_path_check:
-							fl2 = open("manifest.yml", 'a')
-							fl2.write(",".join(rule_path_check))
-							#fl2.write(":")
-							fl2.write("\n")
-						rule_ph_check = rule_ph.findall(fl)
-						if rule_ph_check:
-							fl2 = open("manifest.yml", 'a')
-							fl2.write(",".join(rule_ph_check))
-							#fl2.write(":")
-							fl2.write("\n")
-						rule_sources_check = rule_sources.findall(fl)
-						if rule_sources_check:
-							fl2 = open("manifest.yml", 'a')
-							fl2.write(",".join(rule_sources_check))
-							#fl2.write(":")
-							fl2.write("\n")
-						rule_targets_check = rule_targets.findall(fl)
-						if rule_targets_check:
-							fl2 = open("manifest.yml", 'a')
-							fl2.write(",".join(rule_targets_check))
-							#fl2.write(":")
-							fl2.write("\n")
-						rule_deps_check = rule_deps.findall(fl)
-						if rule_deps_check:
-							fl2 = open("manifest.yml", 'a')
-							fl2.write(",".join(rule_deps_check))
-							#fl2.write(":")
-							fl2.write("\n")
+								fl2.write("\n")
+							rule_package_url_check = rule_package_url.findall(fl)
+							if rule_package_url_check:
+								fl2 = open("manifest.yml", 'a')
+								fl2.write(",".join(rule_package_url_check))
+								#fl2.write(":")
+								fl2.write("\n")
+							rule_ph_check = rule_ph.findall(fl)
+							if rule_ph_check:
+								fl2 = open("manifest.yml", 'a')
+								fl2.write(",".join(rule_ph_check))
+								#fl2.write(":")
+								fl2.write("\n")
+							rule_sources_check = rule_sources.findall(fl)
+							if rule_sources_check:
+								fl2 = open("manifest.yml", 'a')
+								fl2.write(",".join(rule_sources_check))
+								#fl2.write(":")
+								fl2.write("\n")
+							rule_targets_check = rule_targets.findall(fl)
+							if rule_targets_check:
+								fl2 = open("manifest.yml", 'a')
+								fl2.write(",".join(rule_targets_check))
+								#fl2.write(":")
+								fl2.write("\n")
+							rule_deps_check = rule_deps.findall(fl)
+							if rule_deps_check:
+								fl2 = open("manifest.yml", 'a')
+								fl2.write(",".join(rule_deps_check))
+								#fl2.write(":")
+								fl2.write("\n")
+							rule_path_check = rule_path.findall(fl)
+							if rule_path_check:
+								fl2 = open("manifest.yml", 'a')
+								fl2.write(",".join(rule_path_check))
+								#fl2.write(":")
+								fl2.write("\n")
+		if os.path.isfile('temp.yml'):
+			os.remove('temp.yml')
 
 	def clean_deps(self):
 		wd = os.getcwd()
