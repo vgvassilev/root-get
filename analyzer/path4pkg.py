37500a2683bcc2f7291bcc7aa3bb79984bed50e0
Oksana Shadura <oksana.shadura@cern.ch>
2018-06-27 12:36:51 +0200

    Fixing walking path procedure (still need to be fixed - details FIXME)

--- analyzer/path4pkg.py
+++ analyzer/path4pkg.py
@@ -9,7 +9,8 @@ class Path4pkg(object):
         super(Path4pkg, self).__init__()
         self.arg = arg
 
-    def path4pkg(self, dirname, directory='', mindepth=2, maxdepth=float('3')):
+    def path4pkg(self, dirname, directory='', mindepth=2, maxdepth=float('inf')):
+        # FIXME: path4pkg is working as expected, but ROUTSOURCES path can't finish with "/"
         directory = os.path.normcase(directory)
         dirname = dirname.lower()
         non_acceptable_dirs = set(['tutorials', 'test', 'interpreter', 'dictpch'])
@@ -18,7 +19,7 @@ class Path4pkg(object):
         for dirpath, dirs, names in os.walk(directory):
             dirs[:] = [d for d in dirs if d not in non_acceptable_dirs]
             depth = dirpath.count(os.path.sep) - root_depth
-            if mindepth <= depth <= maxdepth:
+            if mindepth <= depth < maxdepth:
                 if((dirname in dirs) or (dirname.strip("io") in dirs)):
                     if(dirname in directory_exceptions):
                         print(dirname)
